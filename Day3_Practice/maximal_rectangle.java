a52c1b85db1c1bae870b45bfb8c7d109fe72375a
Aryamanporwal <aryaman.3307@gmail.com>
2025-09-30 00:43:40 +0530

    maximal_reactangle

--- Day3_Practice/maximal_rectangle.java
+++ Day3_Practice/maximal_rectangle.java
@@ -15,48 +15,46 @@ public class maximal_rectangle {
                         heights[j] += 1;
                     } else {
                         heights[j] = 0;
-                    } 
-                    
+                    }
                 }
-                
                 maxArea = Math.max(maxArea, largestrectangle(heights));
             }
+
             return maxArea;
         }
-        public int largestrectangle(int []arr){
+
+        public int largestrectangle(int[] arr) {
             int n = arr.length;
             Stack<Integer> st = new Stack<>();
             int area = 0;
-            for(int i =0 ; i<n; i++){
-                while(!st.isEmpty() && arr[i] > arr[st.peek()]){
-                    int h = st.pop();
+
+            for (int i = 0; i < n; i++) {
+                while (!st.isEmpty() && arr[i] < arr[st.peek()]) { 
+                    int h = arr[st.pop()];
                     int r = i;
-                    if(st.isEmpty()){
-                        area = Math.max(area , h*r);
-                    }else{
+                    if (st.isEmpty()) {
+                        area = Math.max(area, h * r);
+                    } else {
                         int l = st.peek();
-                        area = Math.max(area , h*(r-l-1));
+                        area = Math.max(area, h * (r - l - 1));
                     }
                 }
                 st.push(i);
             }
-            int r = arr.length;
-            while(!st.isEmpty()){
-                int h = st.pop();
-                if(st.isEmpty()){
-                    area = Math.max(area ,h*r);
-                }
-                else{
+
+            int r = n;
+            while (!st.isEmpty()) {
+                int h = arr[st.pop()];
+                if (st.isEmpty()) {
+                    area = Math.max(area, h * r);
+                } else {
                     int l = st.peek();
-                    area = Math.max(area, h*(r-l-1));
+                    area = Math.max(area, h * (r - l - 1));
                 }
-            
             }
 
             return area;
-
         }
-
-
     }
+
 }
