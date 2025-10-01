c18818382f43994adbcd44df3090fbe2db3fb495
Aryamanporwal <aryaman.3307@gmail.com>
2025-10-01 11:58:34 +0530

    Day5 Practice

---
+++ Day5_Practice/two_chocolates.java
@@ -0,0 +1,12 @@
+package Day5_Practice;
+
+import java.util.Arrays;
+
+public class two_chocolates {
+    class Solution {
+        public int buyChoco(int[] prices, int money) {
+            Arrays.sort(prices);
+            return money - (prices[1]+prices[0]) >=0 ? money - (prices[1]+prices[0]) : money;
+        }
+}
+}
