3e955452da9b0406c4466a973c894dfb106e6df0
Aryamanporwal <aryaman.3307@gmail.com>
2025-10-02 20:56:01 +0530

    Gas station

---
+++ Day5_Practice/gasStation.java
@@ -0,0 +1,25 @@
+package Day5_Practice;
+
+public class gasStation {
+    class Solution {
+        public int canCompleteCircuit(int[] gas, int[] cost) {
+            int total=0;
+            for(int i =0 ; i<cost.length ; i++){
+                total +=gas[i]-cost[i];
+            }
+            if(total<0){
+                return -1;
+            }
+            int idx = 0;
+            int curr = 0 ;
+            for(int i = 0 ; i<cost.length ; i++){
+                curr+= gas[i]-cost[i];
+                if(curr<0){
+                    curr = 0;
+                    idx = i+1;
+                }
+            }
+            return idx;
+        }
+    }
+}
--- Day5_Practice/job_sequencing_problem.java
+++ Day5_Practice/job_sequencing_problem.java
@@ -6,43 +6,50 @@ import java.util.Arrays;
 public class job_sequencing_problem {
     class Solution {
         public ArrayList<Integer> jobSequencing(int[] deadline, int[] profit) {
-            // code here
-            int n = deadline.length;
-            int [][] jobs = new int[n][2];
+            int n = profit.length;
+            int jobs[][] = new int[n][2];
+
             for(int i = 0; i<n ; i++){
                 jobs[i][0] = deadline[i];
                 jobs[i][1] = profit[i];
             }
-            
-            Arrays.sort(jobs, (a,b)->(b[1]-a[1]));
-            //target ye h , jiski deadline km ho or profit jyda ho
-            boolean dead[] = new boolean[n];
-            int maxProfit = 0;
-            int maxJob = 0;
-            for(int i = 0; i<n  ; i++){
-                if(dead[jobs[i][0]-1] == false){
-                    dead[jobs[i][0]-1] = true;
-                    maxProfit += jobs[i][1];
-                    maxJob++;
+
+            Arrays.sort(jobs ,(a,b)->(b[1]-a[1]));
+
+            int maxDeadline = 0;
+            for(int i =0 ; i<n ; i++){
+                maxDeadline = Math.max(maxDeadline , deadline[i]);
+            }
+
+            int slot[] = new int[maxDeadline+1];
+            for(int i =0; i< maxDeadline+1 ; i++){
+                slot[i] = i;
+            }
+
+            int maxprofit = 0;
+            int maxJobs = 0;
+            for(int i =0 ; i<n ;i++){
+                int prof = jobs[i][1];
+                int dl = jobs[i][0];
+
+                int availSlots = find(slot , dl);
+                if(availSlots > 0){
+                    maxprofit += prof;
+                    maxJobs++;
+                    slot[availSlots] = find(slot , availSlots-1);
                 }
-                else if(dead[jobs[i][0]-1] == true){
-                    int temp = jobs[i][0]-2;
-                    while(temp>=0){
-                    if(dead[temp] == false){
-                        dead[temp] = true;
-                        maxProfit += jobs[i][1];
-                        maxJob++;
-                        break;
-                        } 
-                        temp--;
-                    }
-                                    
-                }
+
             }
             ArrayList<Integer> res = new ArrayList<>();
-            res.add(maxJob);
-            res.add(maxProfit);
+            res.add(maxJobs);
+            res.add(maxprofit);
             return res;
+        }
+
+        public int find(int [] slots , int s){
+            if(slots[s] == s) return s;
+            return slots[s] = find(slots , slots[s]);
         }
     }
 }
+
