a04bfb1855872bd341d10480ddc2a3c1663d3905
Aryamanporwal <aryaman.3307@gmail.com>
2025-10-09 00:07:20 +0530

    solved

---
+++ Assignment_9/delete_duplicate_node.java
@@ -0,0 +1,28 @@
+package Assignment_9;
+
+public class delete_duplicate_node {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+ 
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            ListNode temp = head;
+            while(temp!=null && temp.next!=null){
+                if(temp.next.val == temp.val){
+                    temp.next = temp.next.next;
+                }
+                if(temp.next == null) return head;
+                if(temp.next.val != temp.val){
+                    temp = temp.next;
+                }
+            }
+            return head;
+        }
+    }
+}
